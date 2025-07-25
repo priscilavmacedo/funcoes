0712655a34207a72e3b306cb0851b9f047b5e65a
priscila vinhas <priscilaspx@hotmail.com>
2025-07-25 11:50:02 -0300

    condicionais

---
+++ 2sem/funcoes/funcoes.js
@@ -0,0 +1,33 @@
+// function somar(a, b) {
+//   return a + b;
+// }
+
+// console.log(somar(2, 5));
+
+// // Exemplo de uso da função somar
+
+// const multiplicar = function (c, d) {
+//   return 5 * 2;
+// };
+// console.log(multiplicar(5, 2));
+
+// function saudacao(pessoa) {
+//   return function (mensagem) {
+//     console.log(`${mensagem}, ${pessoa}!`);
+//   };
+// }
+
+// const cumprimento = saudacao("Priscila");
+
+// cumprimento("Olá");
+// cumprimento("Bom dia");
+
+// const apresentarUsuario = ({ nome, idade }) => {
+//   console.log(`Nome: ${nome}, Idade: ${idade}`);
+// };
+
+// const usuario = { nome: "João", idade: 30 };
+// apresentarUsuario(usuario);
+
+const multiplicar = (a, b) => a * b;
+console.log(multiplicar(4, 5));
